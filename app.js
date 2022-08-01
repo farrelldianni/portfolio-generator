0bfae77d29a45a8d7556f4b2647bcb579db4cf20
frl <103608522+farrelldianni@users.noreply.github.com>
2022-08-01 14:31:41 -0400

    added inquirer

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js')
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log('portfolio complete. Check index.html');   
+//     }
+// );
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('portfolio complete. Check index.html');   
-    }
-);
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
